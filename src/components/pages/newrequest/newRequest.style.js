aa7ed9a15348c9e0d0eb743296f5df00aa537ec7
Adam Bystran <bys.adam@gmail.com>
2021-02-07 13:29:40 +0000

    Design create

--- src/components/pages/newrequest/newRequest.style.js
+++ src/components/pages/newrequest/newRequest.style.js
@@ -2,12 +2,12 @@ import makeStyles from '@material-ui/core/styles/makeStyles';
 
 export const styles = makeStyles((theme) => ({
     root: {
-        width: "calc(100% - 10px)",
+        width: "100%",
         height: "auto",
         backgroundColor: "#CBF1D2",
         borderRadius: 20,
-        padding: 20,
-        margin: 5,
+        padding: "0px 25px 40px 25px",
+        margin: "18px 0px",
     },
     dropdownPanel: {
         paddingTop: 20,
